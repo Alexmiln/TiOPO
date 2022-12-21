60973618ff9e5c836dfc6c6dcc0018b2758db96e
Alexmiln <alexmiln04@gmail.com>
2022-12-21 21:48:00 +0300

    Добавил тесты для 2 лабораторной

--- lw2/main.py
+++ lw2/main.py
@@ -15,103 +15,119 @@ valid_links = set()
 invalid_links = set()
 
 main_link = 'http://links.qatl.ru/'
-# main_link = 'http://lasdsadinks.qatl.ru/'
+# main_link = 'https://duet-m.ru/'
 # main_link = 'https://www.travelline.ru/'
 
 class LINK:
-	url: str
-	status: int
+    url: str
+    status: int
 
-	def __init__(self, url, status):
-		self.url = url
-		self.status = status
+    def __init__(self, url, status):
+        self.url = url
+        self.status = status
 
 
-def getAbsoluteLink(main: str, found_link: str, ):
-	if found_link == '#':
-		return main
+def getAbsoluteLink(main: str, found_link: str):
+    if found_link == '#':
+        return main
 
-	if found_link is None:
-		return None
+    if found_link is None:
+        return None
 
-	if main in found_link:
-		return found_link
+    if found_link.startswith(main):
+        return found_link
 
-	if found_link.startswith(('http', 'ftp', 'tel:', 'mailto:')):
-		return None
+    if found_link.startswith(('http', 'ftp', 'tel:', 'mailto:', 'javascript')):
+        return None
 
-	return main_link + found_link
+    return main_link + found_link
 
 
 def parseLinks(url: str):
-	reqs = requests.get(url)
-	soup = BeautifulSoup(reqs.text, 'html.parser')
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
 
-	for link in soup.find_all('a'):
-		if url_normalize(getAbsoluteLink(url, link.get('href'))) is not None:
-			found_urls.add(url_normalize(getAbsoluteLink(url, link.get('href'))))
+    for link in soup.find_all('a'):
+        if url_normalize(getAbsoluteLink(url, link.get('href'))) is not None:
+            found_urls.add(url_normalize(getAbsoluteLink(url, link.get('href'))))
 
 
 def checkLink(url: str):
-	global to_visit_urls
-	global checked_urls
-	code = requests.head(url).status_code
-	link = LINK(url, code)
-	result.add(link)
-	checked_urls.add(url)
-	if code < 400:
-		to_visit_urls.add(url)
-	else:
-		visited_urls.add(url)
+    global to_visit_urls
+    global checked_urls
+    code = requests.get(url).status_code
+    link = LINK(url, code)
+    result.add(link)
+    checked_urls.add(url)
+    print('STATUS CODE: ', code, '\n')
+    if code < 400:
+        to_visit_urls.add(url)
+    else:
+        visited_urls.add(url)
+
+
+def not_forbidden_content_type(url: str) -> bool:
+    types = {"mp4", "pdf", "doc", "docx", "xlsx", "xls", "ppt", "pptx", "jpeg", "jpg", "png", "rar", "7z", "zip",
+             "webp", "webm", "gif"}
+    for type in types:
+        if url.find(type) != -1:
+            return False
+
+    return True
 
 
 def checkAllUrls(url):
-	i = 1
-	parseLinks(url)
-	visited_urls.add(url)
-	for link in found_urls:
-		if link not in checked_urls:
-			print('CHECKING: ', link)
-			checkLink(link)
-		if link in to_visit_urls:
-			if link not in visited_urls:
-				url = link
+    if not_forbidden_content_type(url):
+        parseLinks(url)
+    visited_urls.add(url)
+    for link in found_urls:
+        if link not in checked_urls:
+            print('CHECKING: ', link)
+            checkLink(link)
+        if link in to_visit_urls:
+            if link not in visited_urls:
+                url = link
 
-		i = i + 1
-
-	if url in to_visit_urls:
-		if url not in visited_urls:
-			checkAllUrls(url)
+    if url in to_visit_urls:
+        if url not in visited_urls:
+            checkAllUrls(url)
 
 
 def writeResults(links, status):
-	path = f"results/{urlparse(main_link).netloc}_{status}_links.txt"
-	os.makedirs(os.path.dirname(path), exist_ok=True)
-	i = 1
-	with open(path, "w") as f:
-		f.write(status + ' links\n')
-		for link in links:
-			f.write(str(i) + '. ' + link.url + ' || ' + str(link.status) + '\n')
-			i = i + 1
-		f.write('Total ' + status + ' links count: ' + str(len(links)) + '\n')
-		f.write('Checking date: ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    path = f"results/{urlparse(main_link).netloc}_{status}_links.txt"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    i = 1
+    with open(path, "w") as f:
+        f.write(status + ' links\n')
+        for link in links:
+            f.write(str(i) + '. ' + link.url + ' || ' + str(link.status) + '\n')
+            i = i + 1
+        f.write('Total ' + status + ' links count: ' + str(len(links)) + '\n')
+        f.write('Checking date: ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
 
 try:
-	print('CHECKING: ', main_link)
-	checkLink(main_link)
+    print('CHECKING: ', main_link)
+    checkLink(main_link)
 
-	if main_link in to_visit_urls:
-		checkAllUrls(main_link)
+    if main_link in to_visit_urls:
+        checkAllUrls(main_link)
 
-		for link in result:
-			if link.status < 400:
-				valid_links.add(link)
-			else:
-				invalid_links.add(link)
+        for link in result:
+            if link.status < 400:
+                valid_links.add(link)
+            else:
+                invalid_links.add(link)
 
-		writeResults(valid_links, 'valid')
-		writeResults(invalid_links, 'invalid')
+        writeResults(valid_links, 'valid')
+        writeResults(invalid_links, 'invalid')
 
 except requests.ConnectionError:
-	print('INVALID MAIN LINK')
+    print('Connection Error')
+    for link in result:
+        if link.status < 400:
+            valid_links.add(link)
+        else:
+            invalid_links.add(link)
+    writeResults(valid_links, 'valid')
+    writeResults(invalid_links, 'invalid')
